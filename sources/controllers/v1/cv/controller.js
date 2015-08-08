8707b0b66445f2ec7ec4a141688a956e5fa7d67a
yanninho <y4nn5m@gmail.com>
2015-08-08 08:29:42 +0200

    simplify process find

--- sources/controllers/v1/cv/controller.js
+++ sources/controllers/v1/cv/controller.js
@@ -6,6 +6,12 @@ var CV = require('../../../models/cv'),
 exports.init = function(req, res, next) {
 	req.resourceName = 'cv';
 	req.maxResult = 1;
+	req.happyRest = {
+		range : {
+			offset : 0,
+		    limit : 1
+		}	    
+	}	
 	next();
 }
 
@@ -19,14 +25,9 @@ exports.endFind = function(req, res, next) {
 
 exports.find = function(req, res, next) {
 	req.count = 1;
-	if (req.happyRest.range && req.happyRest.range.limit === req.maxResult && res > 0) {
-		req.happyRest.range.limit = req.count;
-	}
-
-	var range = req.happyRest.range;
 	var mongoReq = CV.find();			
 	//add range
-	mongoReq.skip(range.offset).limit(range.limit - range.offset);
+	mongoReq.skip(0).limit(1);
 	mongoReq.exec().then(function(res) {
 		req.result = res;
 		next();
--- sources/controllers/v1/cv/index.js
+++ sources/controllers/v1/cv/index.js
@@ -6,6 +6,6 @@ var router = express.Router();
 var happyRestFields = require('happyrestfields');
 var happyRestRange = require('happyrestrange');
 
-router.get('/', controller.init, happyRestRange.extractRange, controller.find, happyRestFields.selectionFields, happyRestRange.setHeader, controller.endFind);
+router.get('/', controller.init, controller.find, happyRestFields.selectionFields, happyRestRange.addAcceptRange, controller.endFind);
 
 module.exports = router;
